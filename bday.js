cac2a2ac2abf2d3b74335b0dd31ebe3c4c044d9e
serereg <serereg@mail.ru>
2021-04-09 15:31:13 +0300

    history link added

--- bday.js
+++ bday.js
@@ -1,4 +1,4 @@
-console.log(`
+console.log(`https://memepedia.ru/ebat-ty-lox/
 
                                                                                                                 .,,,*,*****/////**..
                                                                                                      .,//(#####(((////////////////**********,.
